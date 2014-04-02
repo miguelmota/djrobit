f7983e99dd60645e06fa52c5ef4f3cda9c325874
Miguel Mota <moogs@Moogs-MacBook-Pro.local>
2014-04-01 19:16:10 -0700

    clean

--- app/scripts/constants.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('robitApp').constant('SOUNDCLOUD', {
-	clientId: 'a5936f4a6e935d8695c2c6e0de46b830'
-}).constant('POSTMARK', {
-	serverToken: 'b5772e33-070e-4bff-a840-704a56f9e373'
-}).constant('INSTAGRAM', {
-	accessToken: '211404734.6ceae3f.59bb5cf01d78429184eed7a20da06383'
-});
