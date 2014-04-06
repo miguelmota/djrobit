9b4309a001ba22feb7bfca63d3472e2e0223524d
Miguel Mota <moogs@Moogs-MacBook-Pro.local>
2014-04-06 16:42:18 -0700

    feed path

--- bin/update_feed.sh
+++ bin/update_feed.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-curl -sS http://djrobit.com/podcast/feed.xml | pickup > $DIR/../app/podcast/feed.json
+curl -sS http://djrobit.com/podcast/feed.xml | pickup > $DIR/../dist/podcast/feed.json
